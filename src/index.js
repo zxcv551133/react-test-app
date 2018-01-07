32cb4a0ae1b937cb257db134eacb8f155dc9b962
zxcv551133 <zxcv551133@gmail.com>
2018-01-07 09:49:29 +0900

    2.2 props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App headerTitle='Welcome!'
+  contentTitle='Stranger,'
+  contentBody='Welcome to example app' />, rootElement)
+registerServiceWorker()
